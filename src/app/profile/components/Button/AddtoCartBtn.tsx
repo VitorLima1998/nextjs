91fc41d7a1ea4cdbabcc53df077b03e746793b00
VitorLima1998 <vitor.xlr8@gmail.com>
2023-11-29 15:37:53 -0400

    Add new component: AddtoCartBtn

---
+++ src/app/profile/components/Button/AddtoCartBtn.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { Button } from 'antd';
+import React from 'react';
+
+function AddtoCartBtn() {
+    return (
+        // Passar como props
+        <div>
+            <Button type="primary" onClick={() => {}}>
+                Add Cart
+            </Button>
+        </div>
+    );
+}
+
+export default AddtoCartBtn;
