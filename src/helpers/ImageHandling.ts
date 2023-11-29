d56ef8547b8d8aeebd0b366f581a1f3918429192
VitorLima1998 <vitor.xlr8@gmail.com>
2023-11-29 15:35:38 -0400

    Add image handling functionality

---
+++ src/helpers/ImageHandling.ts
@@ -0,0 +1,25 @@
+import { firebaseApp } from '@/firebaseConfig';
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+
+export const uploadImageAndReturnUrls = async (files: any) => {
+    try {
+        const imageRefs = await Promise.all(
+            files.map((file: any) => {
+                const storage = getStorage(firebaseApp);
+                const storageRef = ref(storage, `products/${file.name}`);
+                return uploadBytes(storageRef, file);
+            })
+        );
+
+        const imageUrls = await Promise.all(
+            imageRefs.map(async (imageRef) => {
+                return await getDownloadURL(imageRef.ref);
+            })
+        );
+
+        return imageUrls;
+    } catch (error: any) {
+        console.error(error);
+        throw error;
+    }
+};
