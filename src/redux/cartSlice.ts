8d30b2d363569090dd98843b264cf0214a0ca90d
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-05 09:40:47 -0400

    Add cartSlice and userSlice to redux store

---
+++ src/redux/cartSlice.ts
@@ -0,0 +1,54 @@
+import { ProductInterface } from '@/interfaces';
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface CartState {
+    cartItems: ProductInterface[];
+    cartTotal: number;
+}
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        cartItems: [],
+        cartTotal: 0,
+    } as CartState,
+    reducers: {
+        AddProductToCart: (
+            state,
+            action: {
+                type: string;
+                payload: ProductInterface;
+            }
+        ) => {
+            state.cartItems.push(action.payload);
+        },
+        RemoveProductToCart: (
+            state,
+            action: {
+                type: string;
+                payload: ProductInterface;
+            }
+        ) => {
+            state.cartItems = state.cartItems.filter(
+                (item) => item.id !== action.payload.id
+            );
+        },
+        EditProductToCart: (
+            state,
+            action: {
+                type: string;
+                payload: ProductInterface;
+            }
+        ) => {
+            state.cartItems = state.cartItems.map((item) =>
+                item.id === action.payload.id ? action.payload : item
+            );
+        },
+        ClearCart: (state) => {
+            state.cartItems = [];
+        },
+    },
+});
+
+export const { AddProductToCart, RemoveProductToCart, EditProductToCart, ClearCart } =
+    cartSlice.actions;
