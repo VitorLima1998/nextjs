41d96f4c850c319e8e19c5f46927c303f6b7b0dc
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-07 20:32:39 -0400

    Remove unused imports and comments in firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { getAnalytics } from 'firebase/analytics';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: 'AIzaSyDSU8cfdDA5_bQqFQ3pv3_nN1hKpT1Y5sc',
     authDomain: 'ecommerce-nextjs-d5449.firebaseapp.com',
@@ -16,6 +11,5 @@ const firebaseConfig = {
     measurementId: 'G-46MQSTS0SB',
 };
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
