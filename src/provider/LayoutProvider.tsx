2ae1c7a0d877fb506cdd0ca23361a1abdeb6cc69
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-07 20:34:00 -0400

    Add logout functionality and fix currentUser state update

--- src/provider/LayoutProvider.tsx
+++ src/provider/LayoutProvider.tsx
@@ -1,16 +1,22 @@
 'use client';
 import { CartState } from '@/redux/cartSlice';
-import { Badge, Popover, message } from 'antd';
+import { Badge, Popover, message, notification } from 'antd';
+import axios from 'axios';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { useDispatch, useSelector } from 'react-redux';
+import { API_URL } from '../app/constants';
+import { SetCurrentUser } from '../redux/userSlice';
+import Loader from '../app/components/Loader';
 
 function LayoutProvider({ children }: { children: React.ReactNode }) {
     const router = useRouter();
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
     const pathName = usePathname();
     const dispatch = useDispatch();
     const [loading, setLoading] = useState(false);
-    const [currentUser, setCurrentUser] = useState('');
+    const { currentUser } = useSelector((state: any) => state.user);
     const { cartItems }: CartState = useSelector((state: any) => state.cart);
 
     const isPrivatePage =
@@ -30,7 +36,7 @@ function LayoutProvider({ children }: { children: React.ReactNode }) {
             <div
                 className="flex gap-2 items-center cursor-pointer text-md"
                 onClick={() => {
-                    router.push('/logout');
+                    onLogout();
                 }}
             >
                 <i className="ri-logout-box-r-line"></i>
@@ -42,19 +48,41 @@ function LayoutProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         1;
         getCurrentUser();
-    }, []);
+    }, [isPrivatePage, pathName]);
 
     useEffect(() => {
         console.log(cartItems);
-        // when the cartItems changes, we will save cartItem into localStorage
         localStorage.setItem('cartItems', JSON.stringify(cartItems));
     }, [cartItems]);
 
+    const onLogout = async () => {
+        try {
+            setLoading(true);
+            removeCookie('token');
+
+            notification.success({ message: 'Logout successful' });
+            dispatch(SetCurrentUser(null));
+            router.push('auth/login');
+        } catch (error: any) {
+            notification.error({ message: ' Error logout' });
+        } finally {
+            setLoading(false);
+        }
+    };
     const getCurrentUser = async () => {
         try {
             setLoading(true);
-            // get axios in backend
-            setCurrentUser('V');
+            if (cookies.token === 'undefined') {
+                throw new Error('Token is not defined! Please login again!');
+            }
+
+            const res = await axios.get(`${API_URL}/auth/current`, {
+                headers: {
+                    Authorization: `Bearer ${cookies.token}`,
+                },
+            });
+
+            dispatch(SetCurrentUser(res.data.data));
         } catch (error: any) {
             message.error(error.message);
         } finally {
@@ -64,38 +92,43 @@ function LayoutProvider({ children }: { children: React.ReactNode }) {
 
     return (
         <div>
-            {isPrivatePage && (
-                <div className="bg-primary py-5 px-5 flex justify-between items-center">
-                    <div className="flex">
-                        <h1 className="text-2xl font-bold text-white">
-                            Frame Shop
-                        </h1>
+            {loading && <Loader />}
+            {isPrivatePage && currentUser && (
+                <>
+                    <div className="bg-primary py-5 px-5 flex justify-between items-center">
+                        <div className="flex">
+                            <h1 className="text-2xl font-bold text-white">
+                                Frame Shop
+                            </h1>
+                        </div>
+                        <div className="flex gap-5 items-center">
+                            <Badge
+                                count={cartItems.length}
+                                className="cursor-pointer"
+                            >
+                                <i
+                                    className="ri-shopping-cart-line text-white text-2xl"
+                                    onClick={() => {
+                                        router.push('/cart');
+                                    }}
+                                ></i>
+                            </Badge>
+                            <Popover
+                                content={content}
+                                title="Title"
+                                trigger="click"
+                            >
+                                <div className="flex justify-center items-center rounded-full h-8 w-8 bg-white">
+                                    {currentUser.name[0]}
+                                </div>
+                            </Popover>
+                        </div>
                     </div>
-                    <div className="flex gap-5 items-center">
-                        <Badge
-                            count={cartItems.length}
-                            className="cursor-pointer"
-                        >
-                            <i
-                                className="ri-shopping-cart-line text-white text-2xl"
-                                onClick={() => {
-                                    router.push('/cart');
-                                }}
-                            ></i>
-                        </Badge>
-                        <Popover
-                            content={content}
-                            title="Title"
-                            trigger="click"
-                        >
-                            <div className="flex justify-center items-center rounded-full h-8 w-8 bg-white">
-                                {currentUser}
-                            </div>
-                        </Popover>
-                    </div>
-                </div>
+                    <div className="p-5">{children}</div>
+                </>
             )}
-            <div className="p-5">{children}</div>
+
+            {!isPrivatePage && children}
         </div>
     );
 }
